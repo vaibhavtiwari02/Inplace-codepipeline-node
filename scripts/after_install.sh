285982ecefa0c6babf960887d7494f54fc6fca86
Vaibhav Tiwari <91326468+vaibhavtiwari02@users.noreply.github.com>
2024-09-24 12:05:00 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm /home/ubuntu/mynodeapp/node_modules/toidentifier/HISTORY.md
 rm -rf /home/ubuntu/mynodeapp/node_modules
-cd /home/ubuntu/nodeapp/
+cd /home/ubuntu/mynodeapp/
 npm install
 
